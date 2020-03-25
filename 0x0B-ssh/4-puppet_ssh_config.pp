3f2e3db7817b149ba2ae0803ec57f290e6ddf89d
marcewp15 <970@holbertonschool.com>
2020-03-25 03:16:07 +0000

    SSH configuration file for the local SSH client with puppet: connect to a server without typing a password.

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Install puppet-lint package
+
+file_line {'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+file_line {'Declare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentifyFile ~/.ssh/holberton',
+}
