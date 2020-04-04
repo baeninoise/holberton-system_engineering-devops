6fb05f83b487d0f3bb2d0b03fdbfd97295925088
marcewp15 <970@holbertonschool.com>
2020-04-04 06:05:16 +0000

    creating a custom HTTP header response with puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,5 +1,5 @@
 # creating a custom HTTP header response
 exec {'HTTP_Header':
-  command  => 'apt-get -y update && apt-get -y install haproxy && echo -e "\nfrontend front\n\tbind *:80\n\tmode http\n\tdefault_backend back\n\nbackend back\n\tbalance roundrobin\n\tserver 970-web-01 35.231.24.237:80 check\n\tserver 970-web-02 54.90.204.128:80 check" >> /etc/haproxy/haproxy.cfg && service haproxy restart',
+  command  => 'apt-get -y update && apt-get -y install nginx && echo "Holberton School second server" > /var/www/html/index.nginx-debian.html && HTTP_H="server_name _;\n\tadd_header X-Served-By $HOSTNAME;" && sed -i "s/server_name _;/$HTTP_H/" /etc/nginx/sites-available/default && service nginx restart',
   provider => shell,
 }
