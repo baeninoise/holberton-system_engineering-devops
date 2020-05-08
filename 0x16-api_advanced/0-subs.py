69768e6d59269eec28df2e0695438c7fef2f5792
marcewp15 <970@holbertonschool.com>
2020-05-08 02:17:20 +0000

    Fix pep8, This function queries the Reddit API and returns the number of subscribers for a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,6 +7,10 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """
+    This function queries the Reddit API and
+    returns the number of subscribers for a given subreddit
+    """
     URL = 'https://www.reddit.com/r/{}/about/.json'.format(subreddit)
     headers = {'User-agent': 'URL'}
     req = requests.get(URL, headers=headers, allow_redirects=False).json()
