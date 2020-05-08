e98dfa68c80ea32b6f59a7a8927daa806e8928c2
marcewp15 <970@holbertonschool.com>
2020-05-08 04:40:46 +0000

    This recursive function queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Recurse it!
+"""
+import json
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=''):
+    """
+    This recursive function queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit
+    """
+    URL = 'https://www.reddit.com/r/{}/hot/.json?after={}'.format(
+        subreddit, after)
+    headers = {'User-agent': 'URL'}
+    req = requests.get(URL, headers=headers, allow_redirects=False).json()
+    try:
+        lists = req.get('data').get('children')
+        for i in lists:
+            print(i.get('data').get('title'))
+    except:
+        return(None)
+
+    after_data = req.get('data').get('after')
+    if after_data is None:
+        return hot_list
+    return recurse(subreddit, hot_list, after)
